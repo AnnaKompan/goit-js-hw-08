e6438cecb1b8670d6d25261bd648c04b6ccf0ebb
AnnaKompan <annakompanch@gmail.com>
2023-01-30 11:30:44 +0200

    js1/js3 modified

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
@@ -18,9 +20,6 @@ function createGalleryItems(items) {
 }
 
 gallery.innerHTML = galleryItem;
-
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
 new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
